e2bf4e05132f0e7b95f151c8f79f8172430da263
Alexandru <rammazzoti2000@gmail.com>
2020-04-01 10:47:33 +0200

    add select_basics solutions

---
+++ 0_SELECT_basics.sql
@@ -0,0 +1,11 @@
+-- 1. Modify it to show the population of Germany
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+-- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark')
+
+-- 3. Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
